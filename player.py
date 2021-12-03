59b368b3d2b1e30b2dab650799f7b09fc24044b6
Eusebiu Popescu <popescusebi1234@yahoo.com>
2021-12-03 10:40:55 +0200

    Add files via upload

---
+++ player.py
@@ -0,0 +1,16 @@
+import math
+import random
+
+
+class Player():
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+    def get_move1(self, game):
+        pass
+
+
+
