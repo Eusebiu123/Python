9e2405a059668eba458f9a1fafe5c6a859a401d6
Eusebiu Popescu <popescusebi1234@yahoo.com>
2021-12-13 14:37:03 +0200

    Add files via upload

--- player.py
+++ player.py
@@ -1,16 +1,101 @@
-import math
-import random
+import math, random
 
 
-class Player():
-    def __init__(self, letter):
-        self.letter = letter
+class Jucator():
+    def __init__(self, litera):
+        self.litera = litera
 
-    def get_move(self, game):
+    def mutare1(self, joc):
         pass
 
-    def get_move1(self, game):
+    def mutare2(self, joc):
         pass
 
 
+class Om(Jucator):
+    def __init__(self, litera):
+        super().__init__(litera)
 
+    def mutare1(self, joc):
+        loc_liber = False
+        valoare = None
+        while not loc_liber:
+            patratel = input(self.litera + ' alege. Scrie in ce patratel (0-9): ')
+            try:
+                valoare = int(patratel)
+                if valoare not in joc.mutari_posibile():
+                    raise ValueError
+                loc_liber = True
+            except ValueError:
+                print('Patratel invalid sau deja ocupat. Alege altul!')
+        return valoare
+
+    def mutare2(self, joc):
+        loc_liber = False
+        valoare = None
+        while not loc_liber:
+            patratel = input(self.litera + ' alege. Scrie in ce patratel (0-9): ')
+            try:
+                valoare = int(patratel)
+                if valoare not in joc.mutari_posibile():
+                    raise ValueError
+                loc_liber = True
+            except ValueError:
+                print('Patratel invalid sau deja ocupat. Alege altul!')
+        return valoare
+
+
+class Random(Jucator):
+    def __init__(self, litera):
+        super().__init__(litera)
+
+    def mutare1(self, joc):
+        patratel = random.choice(joc.mutari_posibile())
+        return patratel
+
+    def mutare2(self, joc):
+        patratel = random.choice(joc.mutari_posibile())
+        return patratel
+
+
+class Ai(Jucator):
+    def __init__(self, litera):
+        super().__init__(litera)
+
+    def mutare1(self, joc):
+        if len(joc.mutari_posibile()) == 9:
+            patratel = random.choice(joc.mutari_posibile())
+        else:
+            patratel = self.minimax(joc, self.litera)['pozitie']
+        return patratel
+
+    def mutare2(self, joc):
+        if len(joc.mutari_posibile()):
+            patratel = random.choice(joc.mutari_posibile())
+            return patratel
+
+    def minimax(self, state, jucator):
+        max_jucator = self.litera
+        oponent = 'O' if jucator == 'X' else 'X'
+        if state.castigatorcurent == oponent:
+            return {'pozitie': None, 'scor': 1 * (state.nrspatiilibere() + 1) if oponent == max_jucator else -1 * (state.nrspatiilibere() + 1)}
+        elif not state.locuri_libere():
+            return {'pozitie': None, 'scor': 0}
+        if jucator == max_jucator:
+            mutare_buna = {'pozitie': None, 'scor': -math.inf}  # vrem sa maximizam
+        else:
+            mutare_buna = {'pozitie': None, 'scor': math.inf}  # vrem sa minimizam
+        for mutare_posibila in state.mutari_posibile():
+            state.fa_mutare(mutare_posibila, jucator)
+            simulare = self.minimax(state, oponent)
+            # aici dam undo la mutare
+            state.tabla[mutare_posibila] = ' '
+            state.castigatorcurent = None
+            simulare['pozitie'] = mutare_posibila  # aici reprezinta viitoarea mutarea optima
+            if jucator == max_jucator:
+                if simulare['scor'] > mutare_buna['scor']:
+                    mutare_buna = simulare
+            else:
+                if simulare['scor'] < mutare_buna['scor']:
+                    mutare_buna = simulare
+        return mutare_buna
